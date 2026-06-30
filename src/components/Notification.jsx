5cf9b3eba4df898874aacfd9e1b4e47663697b00
ERRAFIQUE ZAKARIA <zakaria.errafique-etu@etu.univh2c.ma>
2026-06-30 19:41:31 +0200

    Update Notification.jsx

--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -1,1 +1,12 @@
+import React, { useEffect } from "react";
+
+function Notification() {
+  useEffect(() => {
+    console.log("Notification loaded");
+  }, []);
+
+  return <div>Appointment Reminder</div>;
+}
+
+export default Notification;
 

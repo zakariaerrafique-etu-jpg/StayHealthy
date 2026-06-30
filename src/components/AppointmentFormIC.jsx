50ce3ec8aa864718cce12b4bbd5fdd8411cf97ea
ERRAFIQUE ZAKARIA <zakaria.errafique-etu@etu.univh2c.ma>
2026-06-30 19:40:04 +0200

    Add AppointmentFormIC component with input fields

--- src/components/AppointmentFormIC.jsx
+++ src/components/AppointmentFormIC.jsx
@@ -1,1 +1,13 @@
+import React from "react";
+
+function AppointmentFormIC() {
+  return (
+    <div>
+      <input placeholder="Name" />
+      <input placeholder="Phone Number" />
+    </div>
+  );
+}
+
+export default AppointmentFormIC;
 
